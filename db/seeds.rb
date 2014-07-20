b190c85e324cc1adf849fdb59b0d12f304729e94
Kudzai Nyandoro <kudzain@gmail.com>
2014-07-20 14:47:31 -0700

    Finish 'Forms: Creating Part I' next go to 'Part II'

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Event.create!([
+  {
+		name: "Kayaking",
+		location: "Victorial Falls, ZW",
+		price: 95.00,
+		description: 'Come kayak with us on the wet and wild Zambezi river',
+		starts_at: 365.days.from_now
+
+  },
+  {
+		name: "Swimming",
+		location: "Fresno, CA",
+		price: 0.00,
+		description: 'Come kayak with us on the wet and wild Zambezi river',
+		starts_at: 365.days.from_now
+  },
+  {
+		name: "Got-gotch",
+		location: "Harare, ZW",
+		price: 50.00,
+		description: 'Come kayak with us on the wet and wild Zambezi river',
+		starts_at: 365.days.from_now
+
+  }
+])
